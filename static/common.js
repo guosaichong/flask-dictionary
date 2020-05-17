25bceb046a18abaf6f302f72aea769c47a01c2f1
guosaichong <510763683@qq.com>
2020-05-17 15:20:22 +0800

    f

--- static/common.js
+++ static/common.js
@@ -1,10 +1,1 @@
 
-
-document.ready(function() {
-    var voice = document.getElementById('voice'); //获取到audio元素
-        document.getElementsByClassName("but").click(function() { //点击文字事件
-        
-            voice.play(); //音乐播放
-            
-        });
-    });
