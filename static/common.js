26cb4855b90452a15ebb68b872a321ca5d30edd3
guosaichong <510763683@qq.com>
2020-05-20 09:20:08 +0800

    f

--- static/common.js
+++ static/common.js
@@ -1,1 +1,9 @@
+function js_method(word){
+    var str='<video controls="" autoplay="" name="media"><source src="http://dict.youdao.com/dictvoice?audio='+word+'&amp;type=1" type="audio/mpeg"></video>'
+    // var parent=document.getElementsByClassName("word");
+    var child=document.createElement("div");
+    child.id="child_div";
+    child.innerHTML=str;
+    child.remove();
+}
 
