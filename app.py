709534a44241f14a2d520454440c9339371b899c
guosaichong <510763683@qq.com>
2020-06-11 22:51:54 +0800

    f

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column,Integer,String,Text,DateTime,UniqueConstraint,Inde
 from sqlalchemy.orm import sessionmaker
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
-import pymysql
+import mysql.connector
 import datetime
 from sqlalchemy.sql.expression import func, select
 from youdao import get_data
@@ -17,7 +17,7 @@ import requests
 app=Flask(__name__)
 
 # 数据库配置
-engine = create_engine("mysql+pymysql://test:123456@47.105.166.136/dictionary?charset=utf8mb4",
+engine = create_engine("mysql+mysqlconnector://test:123456@47.105.166.136/dictionary?charset=utf8mb4",
     max_overflow=0, #超过连接池大小外最多创建的连接
     pool_size=50, # 连接池大小
     pool_timeout=30, # 池中没有线程最多等待的时间，否则报错
