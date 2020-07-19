cd24d37c78585a21a3a28309c4f2e7c302cca89a
guosaichong <510763683@qq.com>
2020-07-19 10:55:32 +0800

    f

--- static/common.js
+++ static/common.js
@@ -1,9 +1,6 @@
 function js_method(word){
     var str='<audio controls="" autoplay="" name="media" src="http://dict.youdao.com/dictvoice?audio='+word+'&amp;type=1" type="audio/mpeg"></audio>'
-    // var parent=document.getElementsByClassName("word");
     var child=document.createElement("div");
-    child.id="child_div";
     child.innerHTML=str;
-    child.remove();
 }
 
