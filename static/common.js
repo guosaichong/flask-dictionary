1295fcb026ce301f6353bcf61bce7896870a0574
guosaichong <510763683@qq.com>
2020-07-23 19:11:09 +0800

    d

--- static/common.js
+++ static/common.js
@@ -2,11 +2,12 @@
 function js_method(word) {
     var txt2 = $("<audio></audio>").attr({ "src": "http://dict.youdao.com/dictvoice?audio=" + word + "&type=1", "autoplay": "autoplay" });
     $(".words").append(txt2);
-    // setTimeout(function(){
-    //     $("audio").remove();
-    //     },3000);
-    if ($("audio").lenght>3) {
-        $("audio").eq(0).remove();
+    var len = $(".words").children().length;
+    // console.log(len);
+    if (len >= 3) {
+        $("audio").eq(0).remove()
     }
+
+
 }
 
